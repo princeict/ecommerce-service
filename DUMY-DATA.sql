6aa274546dddd8fbea8dc19c50403ed4d0ef4cf0
Prince Dey <princedey@businessaccelerate.com.bd>
2024-07-18 04:34:41 +0600

    generate dummy data, a sql file has added

---
+++ DUMY-DATA.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.wish_lists (created_at,created_by,updated_at,updated_by,customer_id,product_id) VALUES
+	 ('2023-01-01 10:00:00',1,'2023-01-01 10:00:00',1,1,1),
+	 ('2023-01-01 10:00:00',1,'2023-01-01 10:00:00',1,1,2);
